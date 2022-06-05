e6ef26a6940710b76bc34e034147753622f092d6
yuvalfishler <yuvalfis@gmail.com>
2022-06-05 11:26:53 -0700

    added opensea testnet

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-export  const nftContractAddress = '0x816473Ba72E84AE1E61aafc8B78dACBc7fdF9bA1';
-
-// 0x52C2B43aF9C85Fa181187a00B9C7B5058a81Bd09
+export  const nftContractAddress = '0x52C2B43aF9C85Fa181187a00B9C7B5058a81Bd09';
 export const NetWorkName = "rinkeby";
 export const NetWorkId = "0x4";
