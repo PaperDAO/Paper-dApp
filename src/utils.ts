ae517abde55b958f1ed694f35f5ec7fd4a8bae81
Asiya <asya.asha@gmail.com>
2022-06-10 13:19:16 -0700

    try another way to open the deep link

--- src/utils.ts
+++ src/utils.ts
@@ -61,10 +61,11 @@ export function isMobileDevice() {
 
 // based on this https://github.com/MetaMask/metamask-mobile/issues/3855
 export function openMetaMaskUrl(url:string) {
-  const a = document.createElement("a");
-  a.href = url;
-  a.target = "_self";
-  document.body.appendChild(a);
-  a.click();
-  a.remove();
+  // const a = document.createElement("a");
+  // a.href = url;
+  // a.target = "_self";
+  // document.body.appendChild(a);
+  // a.click();
+  // a.remove();
+  window.location.href = url;
 }
