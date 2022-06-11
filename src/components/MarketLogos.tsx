04519848073080f5959d65ff4868acd75105b4b7
Roy <toledoroy@gmail.com>
2022-06-10 18:16:11 -0700

    Reorganized the footer links

--- src/components/MarketLogos.tsx
+++ src/components/MarketLogos.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Stack, Link } from "@chakra-ui/react";
+import { Image, Stack, Link, Box } from "@chakra-ui/react";
 import osLogo from '../media/opensea.png'; 
 import rarLogo from '../media/looksrare.png'; 
 import graphLogo from '../media/graph.png'; 
@@ -11,41 +11,49 @@ type MarketLogosProps = {
 
 export default function MarketLogos({ nftContractAddress }: MarketLogosProps) {
   return (
-    <Stack
-      marginTop={75}
-      direction='row'
-      width='100%'
-      background="gray.200"
+    <Box 
       px={6}
       py={6}
-      justifyContent='space-evenly'>
-      <Link isExternal href={`https://twitter.com/whitepaper_dao`}>
+      mt={10}
+      width='100%' 
+      background="gray.200"
+      >
+      <Stack
+        marginTop={75}
+        direction='row'
+        maxWidth='500px'
+        background="gray.200"
+        m="0 auto"
+        justifyContent='space-between'
+        >
+        <Link isExternal href={`https://twitter.com/whitepaper_dao`}>
+          <Image
+            boxSize='50px'
+            objectFit='cover'
+            src={twitterLogo}
+            alt='rar'
+          />
+        </Link>
+        <Link
+          href={`https://testnets.opensea.io/collection/whitepaper-zew70fojy4`}
+          isExternal
+        >
         <Image
           boxSize='50px'
           objectFit='cover'
-          src={twitterLogo}
+          src={osLogo}
+          alt='OpenSea'
+        />
+      </Link>
+      <Link isExternal href={`https://thegraph.com/hosted-service/subgraph/paperdao/whitepaper`}>
+        <Image
+          boxSize='50px'
+          objectFit='cover'
+          src={graphLogo}
           alt='rar'
         />
       </Link>
-      <Link
-        href={`https://testnets.opensea.io/collection/whitepaper-zew70fojy4`}
-        isExternal
-      >
-      <Image
-        boxSize='50px'
-        objectFit='cover'
-        src={osLogo}
-        alt='OpenSea'
-      />
-    </Link>
-    <Link isExternal href={`https://thegraph.com/hosted-service/subgraph/paperdao/whitepaper`}>
-      <Image
-        boxSize='50px'
-        objectFit='cover'
-        src={graphLogo}
-        alt='rar'
-      />
-    </Link>
-  </Stack>
+    </Stack>
+  </Box>
   )
 };
