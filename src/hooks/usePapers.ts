ee60ac9053b38ea4e455ca7041c1e7742b77bf64
Roy <toledoroy@gmail.com>
2022-11-17 23:15:14 -0800

    [REFAC] Less code

--- src/hooks/usePapers.ts
+++ src/hooks/usePapers.ts
@@ -2,20 +2,18 @@ import { useQuery } from "react-query";
 import { gql, request } from 'graphql-request';
 import { getPaperMetadata } from "../utils";
 import { Paper } from "../Router";
-import { useEffect, useState } from "react";
 
 /**
  * Fetch Whitepapers from Subgraph
  */
 export default function usePapers(first: number = 100, skip: number = 0): any {
-    const [papers, setPapers] = useState<Array<Paper>>([]);
     const {
-        data,
+        data: papers,
         isLoading,
         refetch,
     } = useQuery('get-all-white-papers', async (): Promise<any> => {
         if (!process.env.REACT_APP_THE_GRAPH_URL) return null;
-        return request(
+        const res = await request(
             process.env.REACT_APP_THE_GRAPH_URL,
             gql`
                     query ($first: Int, $skip: Int) {
@@ -33,20 +31,11 @@ export default function usePapers(first: number = 100, skip: number = 0): any {
                 first,
                 skip,
             },
-            
         );
-    });
-
-    useEffect(() => {
-        isLoading && setPapers([]);
         //Process Reqults
-        let items = data?.whitepapers || [];
-        items = items.map((paper: Paper) => {
-            return {...paper, metadata:getPaperMetadata(paper)};
-        });
-        // console.log("usePapers() "+items.length+" Set Papers ", {items, isLoading, data});
-        setPapers(items);
-      },[data, isLoading]);
+        let items = res?.whitepapers || [];
+        return items.map((paper: Paper) => ({...paper, metadata:getPaperMetadata(paper)}));
+    });
 
     return { 
         papers,
